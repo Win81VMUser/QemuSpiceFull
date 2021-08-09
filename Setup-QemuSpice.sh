904f7d74bbdb5673d325f9de2abd40c3247eebdb
Win81VMUser <78744628+Win81VMUser@users.noreply.github.com>
2021-08-09 21:48:53 +0200

    Update Setup-QemuSpice.sh

--- Setup-QemuSpice.sh
+++ Setup-QemuSpice.sh
@@ -1,16 +1,9 @@
 echo "This will install Qemu Spice ON TERMUX NO LINUX OS!!! 500mb of space will be used!"
-wget https://github.com/Win81VMUser/QemuSpiceFull/releases/download/v1.0/qemu-spice-4.2.zip
-pkg install unzip -y
 echo "Installing qemu..."
-unzip qemu-spice-4.2.zip
-dpkg -i qemu-spice-4.2/*
-dpkg --configure -a
-apt --fix-broken install
-apt install liblz4
-echo "Removing TEMP files..."
-apt remove unzip -y
-rm -rf *
+
+pkg install qemu-system-x86_64-headless -y
+cleae
 echo "Done! Now download aSpice app and have fun! Starting test command..."
 echo "Press ctrl-c to exit!"
-qemu-system-x86_64 -spice port=5900,addr=127.0.0.1,disable-ticketing $@ -monitor stdio
+qemu-system-x86_64 -spice port=5900,disable-ticketing $@ -monitor stdio
 echo "Spice is installed! Have fun!"
